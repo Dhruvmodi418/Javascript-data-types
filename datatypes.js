8caf41ad087a855476496d32814bfd34723195bf
Dhruvmodi418 <125945328+Dhruvmodi418@users.noreply.github.com>
2026-02-05 20:43:56 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,35 @@
+
+var a = 5;
+// a = a + 1
+let b = 6;
+let c = "Harry";
+let _a = "Shubham";
+
+console.log(b + a)
+console.log(typeof c, typeof a, typeof _a)
+
+
+
+let d = {
+    "job" : "software engineer"
+}
+
+let o = {
+    "name": "Harry",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(d)
+console.log(o)
+
+
+
+console.log(o);
+o.salary = "100crores";
+// first one is to input a data into the data 
+console.log(o);
+
+// this one is to update the data into the data
+o.salary = "500crores";
+console.log(o);
